3ee2eb7a3928652530a4663bdb365662d659301f
ZCS2016 <basketbasket0@126.com>
2018-05-21 16:10:24 +0800

    add PictureDownloadService

--- src/main/java/com/example/CrawlerTest/crawler/picture/wallpaper/linuxwallpaper/writer/LinuxWallpaperWriter.java
+++ src/main/java/com/example/CrawlerTest/crawler/picture/wallpaper/linuxwallpaper/writer/LinuxWallpaperWriter.java
@@ -1,6 +1,7 @@
 package com.example.CrawlerTest.crawler.picture.wallpaper.linuxwallpaper.writer;
 
 import com.example.CrawlerTest.crawler.picture.wallpaper.linuxwallpaper.entity.LinuxWallpaper;
+import com.example.CrawlerTest.crawler.util.io.download.PictureDownloadService;
 import org.springframework.batch.item.ItemWriter;
 
 import java.util.List;
@@ -8,6 +9,12 @@ import java.util.List;
 public class LinuxWallpaperWriter implements ItemWriter<LinuxWallpaper> {
     @Override
     public void write(List<? extends LinuxWallpaper> list) throws Exception {
-
+        for(LinuxWallpaper linuxWallpaper:list){
+            String src = linuxWallpaper.getSrc();
+            String title = linuxWallpaper.getTitle();
+            String fileType = src.substring(src.lastIndexOf("."));
+            title += fileType;
+            PictureDownloadService.downloadPicture(src,title,"PIC");
+        }
     }
 }
---
+++ src/main/java/com/example/CrawlerTest/crawler/util/io/download/PictureDownloadService.java
@@ -0,0 +1,28 @@
+package com.example.CrawlerTest.crawler.util.io.download;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+
+public class PictureDownloadService {
+
+    public static void downloadPicture(String urlStr,String fileName,String localDir){
+        String filePathName = localDir + File.separator + fileName;
+        File localFile = new File(filePathName);
+        try {
+            File dir = new File(localDir);
+            if(!dir.exists()){
+                dir.mkdirs();
+            }
+
+            URL url=new URL(urlStr);
+            FileUtils.copyURLToFile(url,localFile);
+        } catch (IOException e) {
+            System.out.println(urlStr+"\t"+"copyURLToFile failed!");
+            PictureDownloadUtil.download(urlStr,filePathName);
+        }
+    }
+
+}
